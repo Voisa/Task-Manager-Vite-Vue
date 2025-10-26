03982f7ac5fd902fe661d8c288b3e0170ed1abe1
SuFfiKs <MDCS05@mail.ru>
2025-10-26 21:41:42 +0300

    com

--- todo-list/vite.config.ts
+++ todo-list/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: './', // Относительный путь для локального использования
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
+  publicDir: 'public',
+});
